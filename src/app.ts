f373cb149c395c9fca4682aa1026493936a560f8
cm-nitesh <nitesh.devnath.cm@gmail.com>
2025-11-16 22:59:42 +0530

    Cors Install In Node js

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import authRoute from './routes/authRoute.js'
+import cors from "cors";
 dotenv.config()
 const app = express();
 
-
+app.use(cors());
 app.use(express.json());
 
 app.use('/api', authRoute)
